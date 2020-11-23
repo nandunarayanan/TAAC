a2272d33da5a12e4b0b4c21e046dd04a21c617e1
nandunarayanan <67416440+nandunarayanan@users.noreply.github.com>
2020-11-23 12:56:57 +0530

    Add files via upload

---
+++ CPPUnitTestFramework/TestEnvironment/AUTRunner/MathLib.h
@@ -0,0 +1,21 @@
+//MathLibrary.h
+#pragma once
+
+namespace MathLibrary
+{
+    class Arithmetic
+    {
+    public:
+        // Returns a + b
+       double Add(double a, double b);
+
+        // Returns a - b
+        double Subtract(double a, double b);
+
+        // Returns a * b
+         double Multiply(double a, double b);
+
+        // Returns a / b
+         double Divide(double a, double b);
+    };
+}
---
+++ CPPUnitTestFramework/TestEnvironment/MathLib.cpp
@@ -0,0 +1,40 @@
+#include "MathLib.h"
+#include <iostream>
+using namespace MathLibrary;
+
+
+// Returns a + b
+        double Arithmetic::Add(double a, double b)
+        {
+        	return a+b;
+        }
+
+        // Returns a - b
+        double Arithmetic::Subtract(double a, double b)
+        {
+        	return a-b;
+        
+        }
+
+        // Returns a * b
+        double Arithmetic::Multiply(double a, double b)
+        {
+        
+        	return a*b;
+        }
+
+        // Returns a / b
+        double Arithmetic::Divide(double a, double b)
+        {
+        
+        	return a/b;
+        }
+        
+
+int main()
+{
+        
+        return 0;
+}
+ 
+ 
